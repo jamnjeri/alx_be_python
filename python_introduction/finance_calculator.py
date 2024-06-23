6fb44d2974b73f476387f96e7d7563b18b75fe0d
Jamila <jamnjeri@gmail.com>
2024-06-23 10:36:16 +0300

    Reverted to using float

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,7 @@
 monthly_income = input("Enter your monthly income:")
 monthly_expenses = input("Enter your total monthly expenses:")
-monthly_savings = int(monthly_income) - int(monthly_expenses)
+monthly_savings = float(monthly_income) - float(monthly_expenses)
 projected_savings = monthly_savings * 12 + ( monthly_savings * 12 * 0.05)
 
-print("Your monthly savings are", monthly_savings)
-print("Projected savings after one year, with interest, is:", int(projected_savings))
+print(f"Your monthly savings are ${int(monthly_savings)}.")
+print(f"Projected savings after one year, with interest, is: ${int(projected_savings)}.")
