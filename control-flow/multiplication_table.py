54bbf9cb026a9c778aba5d32c6874746069694d8
jamnjeri <jamnjeri@gmail.com>
2024-06-26 17:15:37 +0300

    Range change

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,4 @@
 X = int(input("Enter a number to see its multiplication table:"))
 
-for Y in range(10):
-    print(f"{X} * {Y+1} = {X * (Y+1)}")
+for Y in range(0,10):
+  print(f"{X} * {Y+1} = {X * (Y+1)}")
