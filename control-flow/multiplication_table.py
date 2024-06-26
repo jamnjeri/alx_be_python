080d5ec0fef64c49a73f6ed400f800c3fe9d6b4a
jamnjeri <jamnjeri@gmail.com>
2024-06-26 17:02:00 +0300

    Changed code to use the 1 to 10 range

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,4 @@
 X = int(input("Enter a number to see its multiplication table:"))
 
-for Y in range(1,11):
-  print(f"{X} * {Y} = {X * Y}")
+for Y in range(1,10):
+  print(f"{X} * {Y+1} = {X * (Y+1)}")
