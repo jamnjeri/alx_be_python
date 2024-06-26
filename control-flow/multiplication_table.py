0e237c83f8642190c031badd362d3e6a20872664
jamnjeri <jamnjeri@gmail.com>
2024-06-26 17:18:15 +0300

    Renamed variable to i

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,4 @@
 X = int(input("Enter a number to see its multiplication table:"))
 
-for Y in range(0,10):
-  print(f"{X} * {Y+1} = {X * (Y+1)}")
+for i in range(1,11):
+  print(f"{X} * {i} = {X * (i)}")
