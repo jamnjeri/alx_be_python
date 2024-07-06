e16b49b5dbbe8f87c00cd6d2b6bf207ff8c03f95
Jamila <jamnjeri@gmail.com>
2024-07-06 11:18:43 +0300

    Removed match case

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,12 @@
 def perform_operation(num1, num2, operation):
-  match (operation):
-   case "add":
-     sum = num1 + num2
-     return sum
-   case "subtract":
-     difference = num1 - num2
-     return difference
-   case "multiply":
-     product = num1 * num2
-     return product
-   case "divide":
-     if(num2 == 0):
-       return "Error: Division by zero"
-     return num1 / num2
+    if operation == "add":
+        return num1 + num2
+    if operation == "subtract":
+        return num1 - num2
+    if operation == "multiply":
+        return num1 * num2
+    if operation == "divide":
+        if num2 == 0:
+            return "Error: Division by zero"
+        return num1 / num2
+    return "Invalid operation"
