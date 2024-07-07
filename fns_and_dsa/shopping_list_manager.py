d27c743c699d44b76f0aabcbdb8c760379f56bf0
Jamila <jamnjeri@gmail.com>
2024-07-07 12:47:51 +0300

    Last checker edits

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -13,12 +13,12 @@ def main():
 
         if choice == '1':
             # Prompt for and add an item
-            item = input("Enter item to add:")
+            item = input("Enter item to add: ").strip()
             shopping_list.append(item)
             print(f"{item} added to shopping list.")
         elif choice == '2':
             # Prompt for and remove an item
-            item = input("Enter item to remove:")
+            item = input("Enter item to remove: ").strip()
             if item in shopping_list:
                 shopping_list.remove(item)
                 print(f"{item} removed from shopping list.")
