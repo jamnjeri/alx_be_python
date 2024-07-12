e1158f4904183cf747fbb325b5f3f9b9e675a18f
jamnjeri <jamnjeri@gmail.com>
2024-07-12 11:37:47 +0300

    Deposit output formatted to print only 1 decimal point

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -5,7 +5,7 @@ class BankAccount:
     
   def deposit(self, amount):
     self.account_balance += amount
-    print(f"Deposited: ${amount}")
+    print(f"Deposited: ${amount:.1f}")
     
   def withdraw(self, amount):
     if self.account_balance >= amount:
