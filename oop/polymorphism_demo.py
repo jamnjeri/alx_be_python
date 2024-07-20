e5e27914b169019b39f6cb8d05b02d4967b4e479
jamnjeri <jamnjeri@gmail.com>
2024-07-20 14:08:59 +0300

    Task 2 first commit

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,25 @@
+import math
+
+class Shape:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        raise NotImplementedError("Subclasses should implement this method")
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        super().__init__(length, width)
+
+    def area(self):
+        area = self.length * self.width
+        return area 
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        area = math.pi * self.radius **2
+        return area
